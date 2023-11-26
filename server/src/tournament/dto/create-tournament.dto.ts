f69b26a554672661a9d70502cb106413b885142a
Prince <princekizhthara@gmail.com>
2023-11-26 10:21:03 +0530

    add tournament service

--- server/src/tournament/dto/create-tournament.dto.ts
+++ server/src/tournament/dto/create-tournament.dto.ts
@@ -1,1 +1,24 @@
-export class CreateTournamentDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Transform } from "class-transformer";
+import { IsDate, IsNumber, IsOptional, IsString } from "class-validator";
+
+export class CreateTournamentDto {
+    @ApiProperty({description: 'Tournament name'})
+    @IsString()
+    name:string
+    
+    @ApiProperty({description: 'Description'})
+    @IsString()
+    description:string
+
+    @ApiProperty({description: 'Start time'})
+    @Transform(({value})=>new Date(value))
+    @IsDate()
+    startTime: Date
+
+
+    @ApiProperty({description: 'category'})
+    @IsOptional()
+    @IsNumber()
+    category?: number
+}
