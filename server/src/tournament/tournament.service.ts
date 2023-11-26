faf77c24e3f2dcae7f690b304d3102acb689854b
Prince <princekizhthara@gmail.com>
2023-11-26 14:40:22 +0530

    add api to join users to tournament

--- server/src/tournament/tournament.service.ts
+++ server/src/tournament/tournament.service.ts
@@ -5,23 +5,24 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Tournament } from './entities/tournament.entity';
 import { Repository } from 'typeorm';
 import { CategoriesService } from 'src/categories/categories.service';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class TournamentService {
   constructor(@InjectRepository(Tournament) private tournamentRepository: Repository<Tournament>, private categoryService: CategoriesService) { }
   async create(createTournamentDto: CreateTournamentDto) {
-    const {category,...rest} = createTournamentDto
+    const { category, ...rest } = createTournamentDto
     const categoryEntity = await this.categoryService.findOne(category)
-    const tournament = this.tournamentRepository.create({...rest,category: categoryEntity})
+    const tournament = this.tournamentRepository.create({ ...rest, category: categoryEntity })
     return this.tournamentRepository.save(tournament)
   }
 
   findAll() {
-    return this.tournamentRepository.createQueryBuilder('tournament').innerJoinAndSelect('tournament.category','category').getMany();
+    return this.tournamentRepository.createQueryBuilder('tournament').innerJoinAndSelect('tournament.category', 'category').leftJoinAndSelect('tournament.users', 'users').getMany();
   }
 
   findOne(id: number) {
-    return this.tournamentRepository.findOneBy({id})
+    return this.tournamentRepository.findOneBy({ id })
   }
 
   update(id: number, updateTournamentDto: UpdateTournamentDto) {
@@ -29,6 +30,17 @@ export class TournamentService {
   }
 
   remove(id: number) {
-    return this.tournamentRepository.delete({id});
+    return this.tournamentRepository.delete({ id });
+  }
+
+  async joinTournament(id: number, user: User) {
+    const tournament = await this.findOne(id)
+    const users = tournament.users ?? []
+    const index = users.findIndex(value => value.id === user.id)
+    if (index === -1) {
+      tournament.users = [...users, user]
+      return this.tournamentRepository.save(tournament)
+    }
+    return tournament
   }
 }
